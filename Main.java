48364d1e38d4e67ec8a82e527d82a81a934a3350
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-03-05 23:24:54 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        MallardDuck md = new MallardDuck();
+        System.out.println("------------");
+        md.display();
+        md.performQuack();
+        md.performSwim();
+        md.performFly();
